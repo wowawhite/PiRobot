2b6d2ab51c134d7c49bafb71b0223e3774030f60
wowawhite <trash_bin@gmx.de>
2018-12-07 01:05:20 +0100

    test git again

---
+++ Step_Chain1.py
@@ -0,0 +1,83 @@
+from time import sleep
+import pigpio
+#no Rpi.gpio!
+
+#bcm pin mode
+DIR = 20     # Direction GPIO Pin
+STEP = 21    # Step GPIO Pin
+SWITCH = 16  # GPIO pin of switch
+
+# Connect to pigpiod daemon
+pi = pigpio.pi()
+
+# Set up pins as an output
+pi.set_mode(DIR, pigpio.OUTPUT)
+pi.set_mode(STEP, pigpio.OUTPUT)
+
+# Set up input switch
+pi.set_mode(SWITCH, pigpio.INPUT)
+pi.set_pull_up_down(SWITCH, pigpio.PUD_UP)#pulldown up?
+
+MODE = (14, 15, 18)   # Microstep Resolution GPIO Pins
+RESOLUTION = {'Full': (0, 0, 0),
+              'Half': (1, 0, 0),
+              '1/4': (0, 1, 0),
+              '1/8': (1, 1, 0),
+              '1/16': (0, 0, 1),
+              '1/32': (1, 0, 1)}
+for i in range(3):
+    pi.write(MODE[i], RESOLUTION['Full'][i])
+#pwm not used anymore
+# Set duty cycle and frequency
+#pi.set_PWM_dutycycle(STEP, 128)  # PWM 1/2 On 1/2 Off
+#pi.set_PWM_frequency(STEP, 500)  # 500 pulses per second
+
+def generate_ramp(ramp):
+    """Generate ramp wave forms.
+    ramp:  List of [Frequency, Steps]
+    """
+    pi.wave_clear()     # clear existing waves
+    length = len(ramp)  # number of ramp levels
+    wid = [-1] * length
+
+    # Generate a wave per ramp level
+    for i in range(length):
+        frequency = ramp[i][0]
+        micros = int(500000 / frequency)
+        wf = []
+        wf.append(pigpio.pulse(1 << STEP, 0, micros))  # pulse on
+        wf.append(pigpio.pulse(0, 1 << STEP, micros))  # pulse off
+        pi.wave_add_generic(wf)
+        wid[i] = pi.wave_create()
+
+    # Generate a chain of waves
+    chain = []
+    for i in range(length):
+        steps = ramp[i][1]
+        x = steps & 255
+        y = steps >> 8
+        chain += [255, 0, wid[i], 255, 1, x, y]
+
+    pi.wave_chain(chain)  # Transmit chain.
+
+
+#runtime loop
+try:
+    while True:
+		#joystick parameter hier rein
+        pi.write(DIR, pi.read(SWITCH))  # Set direction
+		# Ramp up, [frequency, duration?]
+		generate_ramp([[320, 200],
+					   [500, 400],
+					   [800, 500],
+					   [1000, 700],
+					   [1600, 900],
+					   [2000, 10000]])
+        sleep(.1) #for what?
+
+
+except KeyboardInterrupt:
+    print ("\nCtrl-C pressed.  Stopping PIGPIO and exiting...")
+finally:
+    pi.set_PWM_dutycycle(STEP, 0)  # PWM off
+    pi.stop()
